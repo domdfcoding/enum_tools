6bc61afcf5fff24d394083b592fd9d9dc40a86f5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-11 14:37:06 +0100

    Bump version: 0.2.1 → 0.3.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 modname = "enum_tools"
 pypi_name = "enum_tools"
 import_name = "enum_tools"
