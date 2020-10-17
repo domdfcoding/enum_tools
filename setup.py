7880595fa08d00b27794902410a34c8e2ec3fbee
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-17 16:17:24 +0100

    Updated config files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description="Tools to expand Python's enum module.",
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
